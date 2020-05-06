bab8a7a6b09675bc36c05629aad86314b6995c39
Warren Buss <warrenkbuss@gmail.com>
2020-05-05 17:44:40 -0700

    add error handling

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css';
 
 class App extends Component {
@@ -25,23 +26,26 @@ class App extends Component {
 
 
     render () {
-        const filteredRobots  = this.state.robots.filter(robots => {
-            return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+        //timesaver -- otherwise its this.state.robots and this.state.searchField in the following
+        const { robots, searchField } = this.state;
+        const filteredRobots  = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchField.toLowerCase());
         })
-        if (this.state.robots.length === 0) {
-            return <h1>Loading...</h1>
-        } else {
-            return (
-                <div className='tc'>
-                    <h1 className='f1'>RoboFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange}/>
-                    <Scroll>
+        // if robots.length === 0, then show loading -> so, 0 means false, we want true, so ! to reverse
+        return !robots.length ?
+            <h1>Loading...</h1> :
+        (
+            <div className='tc'>
+                <h1 className='f1'>RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <Scroll>
+                    <ErrorBoundary>
                         <CardList robots={filteredRobots} />
-                    </Scroll>
-                </div>
-            );
-        }
-    } 
+                    </ErrorBoundary>
+                </Scroll>
+            </div>
+        );
+    }
 }
 
 export default App;
